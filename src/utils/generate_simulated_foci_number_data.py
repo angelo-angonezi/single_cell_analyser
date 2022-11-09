5e67d57aa695ca875c5cd3baa3320134c00c9fe4
angelo-angonezi-win <angeloangonezi2@gmail.com>
2022-11-09 15:58:32 -0300

    finished generate_simulated_foci_number_data.py module

--- src/utils/generate_simulated_foci_number_data.py
+++ src/utils/generate_simulated_foci_number_data.py
@@ -10,6 +10,9 @@ print('initializing...')  # noqa
 
 print('importing required libraries...')  # noqa
 from time import sleep
+from os import listdir
+from random import randint
+from pandas import DataFrame
 from argparse import ArgumentParser
 print('all required libraries successfully imported.')  # noqa
 sleep(0.8)
@@ -32,21 +35,25 @@ def get_args_dict() -> dict:
     # adding arguments to parser
     parser.add_argument('-i', '--images-input-folder',
                         dest='images_input_folder',
+                        type=str,
                         help='defines path to folder containing images',
                         required=True)
 
     parser.add_argument('-o', '--output-path',
                         dest='output_path',
+                        type=str,
                         help='defines path to output csv file containing simulated data',
                         required=True)
 
     parser.add_argument('-n', '--foci-min',
                         dest='foci_num_min',
+                        type=int,
                         help='defines minimum number of foci in simulated data',
                         required=True)
 
     parser.add_argument('-m', '--foci-max',
                         dest='foci_num_max',
+                        type=int,
                         help='defines maximum number of foci in simulated data',
                         required=True)
 
@@ -78,7 +85,50 @@ def simulate_foci_data(images_folder_path: str,
     :param foci_num_max: Integer. Represents number of foci in a given image.
     :return: None.
     """
-    pass
+    # printing execution message
+    f_string = f'getting images in input folder...'
+    print(f_string)
+
+    # getting images in input folder
+    images_in_folder = listdir(images_folder_path)
+
+    # getting images names
+    images_names = [img_name.replace('.tif', '')
+                    for img_name
+                    in images_in_folder
+                    if img_name.endswith('.tif')]
+
+    # getting number of images
+    images_num = len(images_names)
+
+    # printing execution message
+    f_string = f'{images_num} images found.'
+    print(f_string)
+
+    # printing execution message
+    f_string = f'creating random foci counts for images...'
+    print(f_string)
+
+    # creating random numbers list (min/max value determined by parameters)
+    foci_nums = [randint(foci_num_min,
+                         foci_num_max)
+                 for _
+                 in images_names]
+
+    # creating output dictionary
+    output_dict = {'image_name': images_names,
+                   'foci_count': foci_nums}
+
+    # creating output dataframe from dictionary
+    output_df = DataFrame(output_dict)
+
+    # saving final df in output path
+    output_df.to_csv(output_path,
+                     index=False)
+
+    # printing execution message
+    f_string = f'simulated foci counts data saved at "{output_path}"'
+    print(f_string)
 
 ######################################################################
 # defining main function
