d4a9648cb26e8e6b63809d7234b58689ec386c9f
angelo-angonezi-win <angeloangonezi2@gmail.com>
2023-02-24 23:01:37 -0300

    changed lucas_xmls to pascal in convert_annotation_from_rolabelimg_xml_subfolders_to_all.py

--- src/utils/converters/convert_annotation_from_rolabelimg_xml_subfolders_to_all.py
+++ src/utils/converters/convert_annotation_from_rolabelimg_xml_subfolders_to_all.py
@@ -19,7 +19,7 @@ from src.utils.aux_funcs import spacer
 
 PATH_TO_CONVERTER_ROLABELIMG_TO_ALPR = 'Z:\\pycharm_projects\\single_cell_analyser\\src\\utils\\converters\\convert_annotation_from_rolabelimg_xml_to_alpr.py'
 PATH_TO_CONVERTER_ALPR_TO_DOTA = 'Z:\\pycharm_projects\\RotationDetection\\dataloader\\dataset\\UFRGS_CELL\\convert_ann2dota.py'
-PATH_TO_CONVERTER_DOTA_TO_LUCAS_XML = 'Z:\\pycharm_projects\\RotationDetection\\dataloader\\dataset\\UFRGS_CELL\\txt2xml.py'
+PATH_TO_CONVERTER_DOTA_TO_PASCAL = 'Z:\\pycharm_projects\\RotationDetection\\dataloader\\dataset\\UFRGS_CELL\\txt2xml.py'
 
 #####################################################################
 # argument parsing related functions
@@ -69,13 +69,13 @@ def get_args_dict() -> dict:
                         required=True,
                         help=dota_help)
 
-    # lucas_xml folder param
-    lucas_xml_help = 'defines output folder '
-    lucas_xml_help += '(folder with subfolders that will contain lucas_xmls[.xml] annotations for each evaluator)'
-    parser.add_argument('-l', '--lucas-xmls-folder',
-                        dest='lucas_xmls_folder',
+    # pascal folder param
+    pascal_help = 'defines output folder '
+    pascal_help += '(folder with subfolders that will contain pascal[.xml] annotations for each evaluator)'
+    parser.add_argument('-p', '--pascal-folder',
+                        dest='pascal_folder',
                         required=True,
-                        help=lucas_xml_help)
+                        help=pascal_help)
 
     # creating arguments dictionary
     args_dict = vars(parser.parse_args())
@@ -121,7 +121,7 @@ def convert_files_in_single_dir(subfolder: str,
                                 rolabelimg_folder_path: str,
                                 alpr_folder_path: str,
                                 dota_folder_path: str,
-                                lucas_xmls_folder_path: str
+                                pascal_folder_path: str
                                 ) -> None:
     """
     Given a path to a subfolder containing annotations in
@@ -131,7 +131,7 @@ def convert_files_in_single_dir(subfolder: str,
     subfolder_path_rolabelimg = join(rolabelimg_folder_path, subfolder)
     subfolder_path_alpr = join(alpr_folder_path, subfolder)
     subfolder_path_dota = join(dota_folder_path, subfolder)
-    subfolder_path_lucas_xml = join(lucas_xmls_folder_path, subfolder)
+    subfolder_path_pascal = join(pascal_folder_path, subfolder)
 
     # creating rolabelimg to alpr command string
     rolabelimg_to_alpr_command = f"python3 '{PATH_TO_CONVERTER_ROLABELIMG_TO_ALPR}' -i '{subfolder_path_rolabelimg}' -o '{subfolder_path_alpr}'"
@@ -140,19 +140,19 @@ def convert_files_in_single_dir(subfolder: str,
     alpr_to_dota_command = f"python3 '{PATH_TO_CONVERTER_ALPR_TO_DOTA}' '{images_folder_path}' '{subfolder_path_alpr}' '{subfolder_path_dota}'"
 
     # creating dota to lucas xmls command string
-    dota_to_lucas_xml_command = f"python3 '{PATH_TO_CONVERTER_DOTA_TO_LUCAS_XML}' '{subfolder_path_dota}' '{subfolder_path_lucas_xml}' '{images_folder_path}'"
+    dota_to_pascal_command = f"python3 '{PATH_TO_CONVERTER_DOTA_TO_PASCAL}' '{subfolder_path_dota}' '{subfolder_path_pascal}' '{images_folder_path}'"
 
     # running commands
     run_command(command_string=rolabelimg_to_alpr_command)
     run_command(command_string=alpr_to_dota_command)
-    run_command(command_string=dota_to_lucas_xml_command)
+    run_command(command_string=dota_to_pascal_command)
 
 
 def convert_files_in_multiple_dirs(images_folder_path: str,
                                    rolabelimg_folder_path: str,
                                    alpr_folder_path: str,
                                    dota_folder_path: str,
-                                   lucas_xmls_folder_path: str
+                                   pascal_folder_path: str
                                    ) -> None:
     """
     Given a path to a folder containing multiple subfolders containing
@@ -176,7 +176,7 @@ def convert_files_in_multiple_dirs(images_folder_path: str,
                                     rolabelimg_folder_path=rolabelimg_folder_path,
                                     alpr_folder_path=alpr_folder_path,
                                     dota_folder_path=dota_folder_path,
-                                    lucas_xmls_folder_path=lucas_xmls_folder_path)
+                                    pascal_folder_path=pascal_folder_path)
 
     # printing execution message
     f_string = 'all annotation files converted!'
@@ -204,7 +204,7 @@ def main():
     dota_folder = args_dict['dota_folder']
 
     # getting lucas xmls folder
-    lucas_xmls_folder = args_dict['lucas_xmls_folder']
+    pascal_folder = args_dict['pascal_folder']
 
     # printing execution message
     f_string = f'--Execution Parameters--\n'
@@ -212,7 +212,7 @@ def main():
     f_string += f'rolabelimg folder: {rolabelimg_folder}\n'
     f_string += f'alpr folder: {alpr_folder}\n'
     f_string += f'dota folder: {dota_folder}\n'
-    f_string += f'lucas xmls folder: {lucas_xmls_folder}'
+    f_string += f'lucas xmls folder: {pascal_folder}'
     spacer()
     print(f_string)
     spacer()
@@ -226,7 +226,7 @@ def main():
                                    rolabelimg_folder_path=rolabelimg_folder,
                                    alpr_folder_path=alpr_folder,
                                    dota_folder_path=dota_folder,
-                                   lucas_xmls_folder_path=lucas_xmls_folder)
+                                   pascal_folder_path=pascal_folder)
 
 ######################################################################
 # running main function
