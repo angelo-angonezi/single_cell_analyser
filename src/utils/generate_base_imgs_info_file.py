074d68dce4cc3f136a50b1eb22ef6cd68179ef96
angelo-angonezi <angeloangonezi2@gmail.com>
2023-03-02 13:18:34 -0300

    added main logic to generate_base_imgs_info_file.py module

--- src/utils/generate_base_imgs_info_file.py
+++ src/utils/generate_base_imgs_info_file.py
@@ -9,6 +9,7 @@ print('initializing...')  # noqa
 
 # importing required libraries
 print('importing required libraries...')  # noqa
+from pandas import DataFrame
 from argparse import ArgumentParser
 from src.utils.aux_funcs import spacer
 from src.utils.aux_funcs import flush_or_print
@@ -46,7 +47,7 @@ def get_args_dict() -> dict:
 
     # test_imgs_folder param
     test_imgs_folder_help = 'defines path to folder containing test images'
-    parser.add_argument('-te', '--train-imgs-folder',
+    parser.add_argument('-te', '--test-imgs-folder',
                         dest='test_imgs_folder',
                         required=True,
                         help=test_imgs_folder_help)
@@ -77,8 +78,65 @@ def get_args_dict() -> dict:
 
 def generate_base_imgs_info_file(train_imgs_folder: str,
                                  test_imgs_folder: str,
+                                 images_extension: str,
+                                 output_path: str
                                  ) -> None:
-    pass
+    """
+    Given a path to train and test images folder,
+    and given images extension, generates base
+    image info file in output path.
+    :param train_imgs_folder: String. Represents a path to a folder.
+    :param test_imgs_folder: String. Represents a path to a folder.
+    :param images_extension: String. Represents image extension.
+    :param output_path: String. Represents output file path.
+    """
+    # getting files in input folders
+    print('getting files in train_imgs_folder...')
+    train_imgs = get_specific_files_in_folder(path_to_folder=train_imgs_folder,
+                                              extension=images_extension)
+    train_imgs_num = len(train_imgs)
+
+    print('getting files in test_imgs_folder...')
+    test_imgs = get_specific_files_in_folder(path_to_folder=test_imgs_folder,
+                                             extension=images_extension)
+    test_imgs_num = len(test_imgs)
+
+    all_imgs = train_imgs.extend(test_imgs)
+    imgs_num = len(all_imgs)
+
+    # printing execution message
+    f_string = f'A total of {imgs_num} images were found in input folders\n'
+    f_string += f'train_imgs_num: {train_imgs_num}'
+    f_string += f'test_imgs_num: {test_imgs_num}'
+    print(f_string)
+
+    # iterating over images
+    for image_index, image_name in enumerate(all_imgs, 1):
+
+        # printing execution message
+        progress_ratio = image_index / imgs_num
+        progress_percentage = progress_ratio * 100
+        progress_percentage_round = round(progress_percentage)
+        progress_string = f'analysing image {image_index} of {imgs_num} ({progress_percentage_round}%)'
+        flush_or_print(string=progress_string,
+                       index=image_index,
+                       total=imgs_num)
+
+        # getting current image data
+        current_img_split = image_name.split('_')
+        current_img_experiment =
+        current_img_author =
+        current_img_cell_line =
+        current_img_treatment =
+        current_img_well =
+        current_img_frame =
+
+        # creating current image dict
+        current_img_dict = {'img_name': image_name}
+
+        # creating current image df
+        current_img_df = DataFrame(current_img_dict,
+                                   index=[0])
 
 ######################################################################
 # defining main function
@@ -89,8 +147,14 @@ def main():
     # getting args dict
     args_dict = get_args_dict()
 
-    # getting input file
-    input_file = args_dict['input_file']
+    # getting train_imgs_folder param
+    train_imgs_folder = args_dict['train_imgs_folder']
+
+    # getting test_imgs_folder param
+    test_imgs_folder = args_dict['test_imgs_folder']
+
+    # getting images_extension param
+    images_extension = args_dict['images_extension']
 
     # printing execution parameters
     print_execution_parameters(params_dict=args_dict)
@@ -99,7 +163,9 @@ def main():
     enter_to_continue()
 
     # running analyse_imgs_info_file function
-    analyse_imgs_info_file(input_file=input_file)
+    generate_base_imgs_info_file(train_imgs_folder=train_imgs_folder,
+                                 test_imgs_folder=test_imgs_folder,
+                                 images_extension=images_extension)
 
 ######################################################################
 # running main function
