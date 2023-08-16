e74ee2b569ac8450f9f896e1deda67916b8d5429
angelo-angonezi-win <angeloangonezi2@gmail.com>
2023-08-16 12:20:11 -0300

    added generate_crops_pixels_df.py module

--- src/utils/generate_pixel_intensity_histograms.py
+++ src/utils/generate_pixel_intensity_histograms.py
@@ -1,4 +1,4 @@
-# generate table from crops module
+# generate pixel intensity histograms module
 
 print('initializing...')  # noqa
 
@@ -34,7 +34,7 @@ def get_args_dict() -> dict:
     :return: Dictionary. Represents the parsed arguments.
     """
     # defining program description
-    description = 'generate pixel intensity histograms for single-cell crops'
+    description = 'generate pixel intensity histograms for single-nuclei crops'
 
     # creating a parser instance
     parser = ArgumentParser(description=description)
@@ -42,36 +42,16 @@ def get_args_dict() -> dict:
     # adding arguments to parser
 
     # red folder param
-    parser.add_argument('-r', '--red-folder',
-                        dest='red_folder',
+    parser.add_argument('-d', '--crops-pixels-df',
+                        dest='crops_pixels_df',
                         required=True,
-                        help='defines red input folder (folder containing crops in fluorescence channel)')
-
-    # green folder param
-    parser.add_argument('-g', '--green-folder',
-                        dest='green_folder',
-                        required=True,
-                        help='defines green input folder (folder containing crops in fluorescence channel)')
-
-    # images extension param
-    parser.add_argument('-x', '--images-extension',
-                        dest='images_extension',
-                        required=True,
-                        help='defines extension (.tif, .png, .jpg) of images in input folders')
-
-    # crops file param
-    crops_help = 'defines path to crops file (containing crops info)'
-    parser.add_argument('-c', '--crops-file',
-                        dest='crops_file',
-                        required=True,
-                        help=crops_help)
+                        help='defines path to file containing info on crops pixels (.csv)')
 
     # output folder param
-    output_help = 'defines output folder (folder that will contain output files)'
     parser.add_argument('-o', '--output-folder',
                         dest='output_folder',
                         required=True,
-                        help=output_help)
+                        help='defines output folder (folder that will contain output files)')
 
     # creating arguments dictionary
     args_dict = vars(parser.parse_args())
@@ -83,142 +63,37 @@ def get_args_dict() -> dict:
 # defining auxiliary functions
 
 
-def get_crops_df(crops_file: str) -> DataFrame:
-    """
-    Given a path to a crops info csv,
-    returns crops data frame.
-    :param crops_file: String. Represents a path to a file.
-    :return: DataFrame. Represents crops info data frame.
-    """
-    # defining col types
-    col_types = {'img_name': str,
-                 'crop_index': int,
-                 'crop_name': str,
-                 'cx': int,
-                 'cy': int,
-                 'width': int,
-                 'height': int,
-                 'angle': float,
-                 'class': str}
-
-    # reading crops file
-    crops_df = read_csv(crops_file,
-                        dtype=col_types)
-
-    # returning crops df
-    return crops_df
-
-
-def generate_pixel_intensity_histograms(red_folder: str,
-                                        green_folder: str,
-                                        images_extension: str,
-                                        crops_file: str,
+def generate_pixel_intensity_histograms(crops_file: str,
                                         output_folder: str,
                                         ) -> None:
     """
     Given a path to a folder containing crops,
     and a path to a file containing crops info,
     generates ML input compatible tables
-    :param red_folder: String. Represents a path to a folder.
-    :param green_folder: String. Represents a path to a folder.
-    :param images_extension: String. Represents image extension.
     :param crops_file: String. Represents a path to a file.
     :param output_folder: String. Represents a path to a folder.
     :return: None.
     """
-    # getting crops df
-    crops_df = get_crops_df(crops_file=crops_file)
+    # reading crops pixels df
+    crops_pixels_df = read_csv(crops_file)
 
-    # getting crops num
-    crops_num = len(crops_df)
+    # generating current crop pair histogram
+    # TODO: add grouping/loop before running this part
+    histplot(data=crops_pixels_df,
+             x='pixel_intensity',
+             hue='channel',
+             hue_order=['red', 'green'],
+             palette=['r', 'g'],
+             kde=False)
 
-    # defining placeholder value for dfs_list
-    dfs_list = []
-
-    # getting df rows
-    df_rows = crops_df.iterrows()
-
-    # defining start value for current_crop_index
-    current_crop_index = 1
-
-    # iterating over df rows
-    for row_index, row_data in df_rows:
-
-        # TODO: remove this after tests!
-        if current_crop_index == 11:
-            break
-
-        # printing execution message
-        f_string = f'generating histogram for crop #INDEX# of #TOTAL#'
-        print_progress_message(base_string=f_string,
-                               index=current_crop_index,
-                               total=crops_num)
-
-        # updating index
-        current_crop_index += 1
-
-        # getting current crop name
-        crop_name = row_data['crop_name']
-        crop_name_w_extension = f'{crop_name}{images_extension}'
-
-        # getting current crop paths
-        red_path = join(red_folder, crop_name_w_extension)
-        green_path = join(green_folder, crop_name_w_extension)
-
-        # getting current crop pixels
-        red_pixels = get_crop_pixels(crop_path=red_path)
-        green_pixels = get_crop_pixels(crop_path=green_path)
-
-        # checking if all pixels are zero
-        # TODO: remove this if unused
-        red_is_zero = np_all(red_pixels == 0)
-        green_is_zero = np_all(green_pixels == 0)
-
-        # assembling current crop pair dict
-        red_list = ['red' for _ in red_pixels]
-        green_list = ['green' for _ in green_pixels]
-        red_list.extend(green_list)
-        red_pixels_list = [pixel for pixel in red_pixels]
-        green_pixels_list = [pixel for pixel in green_pixels]
-        red_pixels_list.extend(green_pixels_list)
-        name_list = [crop_name for _ in red_pixels_list]
-        current_dict = {'crop_name': name_list,
-                        'channel': red_list,
-                        'pixel_intensity': red_pixels_list}
-
-        # assembling current crop pair df
-        current_df = DataFrame(current_dict)
-
-        # appending current df to dfs_list
-        dfs_list.append(current_df)
-
-        # generating current crop pair histogram
-        histplot(data=current_df,
-                 x='pixel_intensity',
-                 hue='channel',
-                 hue_order=['red', 'green'],
-                 palette=['r', 'g'],
-                 kde=False)
-
-        # saving plot
-        save_name = f'{crop_name}.png'
-        save_path = join(output_folder,
-                         save_name)
-        plt.savefig(save_path)
-
-        # closing plot
-        plt.close()
-
-    # concatenating dfs in dfs_list
-    final_df = concat(dfs_list,
-                      ignore_index=True)
-
-    # saving final df
-    save_name = f'crops_pixels_df.csv'
+    # saving plot
+    save_name = f'crop.png'
     save_path = join(output_folder,
                      save_name)
-    final_df.to_csv(save_path,
-                    index=False)
+    plt.savefig(save_path)
+
+    # closing plot
+    plt.close()
 
     # printing execution message
     print(f'files saved to {output_folder}')
@@ -233,17 +108,8 @@ def main():
     # getting args dict
     args_dict = get_args_dict()
 
-    # getting red folder
-    red_folder = args_dict['red_folder']
-
-    # getting green folder
-    green_folder = args_dict['green_folder']
-
-    # getting image extension
-    images_extension = args_dict['images_extension']
-
-    # getting crops file
-    crops_file = args_dict['crops_file']
+    # getting crops pixels file
+    crops_file = args_dict['crops_pixels_df']
 
     # getting output folder
     output_folder = args_dict['output_folder']
@@ -255,10 +121,7 @@ def main():
     enter_to_continue()
 
     # running generate_pixel_intensity_histograms function
-    generate_pixel_intensity_histograms(red_folder=red_folder,
-                                        green_folder=green_folder,
-                                        images_extension=images_extension,
-                                        crops_file=crops_file,
+    generate_pixel_intensity_histograms(crops_file=crops_file,
                                         output_folder=output_folder)
 
 ######################################################################
