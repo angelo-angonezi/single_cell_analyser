b6464365e08e86abcd1cc5a57dcf99c41bf8f78c
angelo-angonezi <angeloangonezi2@gmail.com>
2024-01-11 16:33:43 -0300

    added get_image_clusters related modules

---
+++ src/utils/get_image_clusters.py
@@ -0,0 +1,432 @@
+# get image clusters module
+
+print('initializing...')  # noqa
+
+# Code destined to obtaining image clusters
+# based on feature extracted from the images.
+
+######################################################################
+# imports
+
+# importing required libraries
+print('importing required libraries...')  # noqa
+from cv2 import imread
+from cv2 import imwrite
+from cv2 import putText
+from cv2 import cvtColor
+from os.path import join
+from numpy import ndarray
+from pandas import Series
+from pandas import DataFrame
+from cv2 import COLOR_BGR2RGB
+from cv2 import COLOR_RGB2BGR
+from argparse import ArgumentParser
+from cv2 import FONT_HERSHEY_SIMPLEX
+from src.utils.aux_funcs import draw_circle
+from src.utils.aux_funcs import draw_ellipse
+from src.utils.aux_funcs import draw_rectangle
+from src.utils.aux_funcs import enter_to_continue
+from src.utils.aux_funcs import print_progress_message
+from src.utils.aux_funcs import print_execution_parameters
+from src.utils.aux_funcs import get_specific_files_in_folder
+from src.utils.aux_funcs import get_merged_detection_annotation_df
+print('all required libraries successfully imported.')  # noqa
+
+#####################################################################
+# defining global variables
+
+
+#####################################################################
+# argument parsing related functions
+
+
+def get_args_dict() -> dict:
+    """
+    Parses the arguments and returns a dictionary of the arguments.
+    :return: Dictionary. Represents the parsed arguments.
+    """
+    # defining program description
+    description = "gets image clusters based on features extracted from images"
+
+    # creating a parser instance
+    parser = ArgumentParser(description=description)
+
+    # adding arguments to parser
+
+    # input folder param
+    input_help = 'defines input folder (folder containing images)'
+    parser.add_argument('-i', '--input-folder',
+                        dest='input_folder',
+                        required=True,
+                        help=input_help)
+
+    # output folder param
+    output_help = 'defines output folder (folder that will contain outlined images)'
+    parser.add_argument('-o', '--output-folder',
+                        dest='output_folder',
+                        required=True,
+                        help=output_help)
+
+    # detection file param
+    detection_help = 'defines path to csv file containing model detections'
+    detection_help += '(if none is passed, adds only fornma annotations)'
+    parser.add_argument('-d', '--detection_file',
+                        dest='detection_file',
+                        required=False,
+                        help=detection_help)
+
+    # gt file param
+    gt_help = 'defines path to csv file containing ground-truth annotations'
+    gt_help += '(if none is passed, adds only model detections)'
+    parser.add_argument('-g', '--ground-truth-file',
+                        dest='ground_truth_file',
+                        required=False,
+                        help=gt_help)
+
+    # image extension param
+    extension_help = 'defines extension (.tif, .png, .jpg) of images in input folder'
+    parser.add_argument('-x', '--images-extension',
+                        dest='images_extension',
+                        required=True,
+                        help=extension_help)
+
+    # detection threshold param
+    threshold_help = 'defines threshold to be applied (filters detections OBBs based on detection threshold)'
+    parser.add_argument('-t', '--detection-threshold',
+                        dest='detection_threshold',
+                        required=False,
+                        default=0.5,
+                        help=threshold_help)
+
+    # overlay style param
+    style_help = 'defines overlay style (rectangle/circle/ellipse)'
+    parser.add_argument('-s', '--overlay-style',
+                        dest='overlays_style',
+                        required=False,
+                        default='ellipse',
+                        help=style_help)
+
+    # creating arguments dictionary
+    args_dict = vars(parser.parse_args())
+
+    # returning the arguments dictionary
+    return args_dict
+
+######################################################################
+# defining auxiliary functions
+
+
+def add_single_overlay(open_img: ndarray,
+                       obbs_df_row: Series,
+                       color_dict: dict,
+                       style: str = 'rectangle'
+                       ) -> None:
+    """
+    Given an open image and respective obb row
+    (extracted from merged_df), returns image
+    with given obb overlay.
+    :param open_img: ndarray. Represents an open image.
+    :param obbs_df_row: Series. Represents single obb data.
+    :param color_dict: Dictionary. Represents colors to be used in overlays.
+    :param style: String. Represents overlays style (rectangle/circle/ellipse).
+    :return: None.
+    """
+    # getting current row bounding box info
+    cx = int(obbs_df_row['cx'])
+    cy = int(obbs_df_row['cy'])
+    width = float(obbs_df_row['width'])
+    height = float(obbs_df_row['height'])
+    angle = float(obbs_df_row['angle'])
+    det_class = str(obbs_df_row['class'])
+    evaluator = str(obbs_df_row['evaluator'])
+
+    # defining color for overlay
+    overlay_color = color_dict[evaluator]
+
+    # defining thickness
+    thickness = 2
+
+    # checking overlay style
+
+    if style == 'rectangle':
+
+        # adding rectangle overlay
+        draw_rectangle(open_img=open_img,
+                       cx=cx,
+                       cy=cy,
+                       width=width,
+                       height=height,
+                       angle=angle,
+                       color=overlay_color,
+                       thickness=thickness)
+
+    elif style == 'circle':
+
+        # getting obb radius
+        radius = (width + height) / 2
+        radius = int(radius)
+
+        # adding circle overlay
+        draw_circle(open_img=open_img,
+                    cx=cx,
+                    cy=cy,
+                    radius=radius,
+                    color=overlay_color,
+                    thickness=thickness)
+
+    elif style == 'ellipse':
+
+        # adding elliptical overlay
+        draw_ellipse(open_img=open_img,
+                     cx=cx,
+                     cy=cy,
+                     width=width,
+                     height=height,
+                     angle=angle,
+                     color=overlay_color,
+                     thickness=thickness)
+
+    # adding class text
+    putText(open_img,
+            det_class,
+            (int(cx), int(cy)),
+            FONT_HERSHEY_SIMPLEX,
+            0.0001,  # TODO: change this once we got more classes and think of better way to display class info
+            overlay_color,
+            2)
+
+
+def add_multiple_overlays(open_img: ndarray,
+                          current_image_df: DataFrame,
+                          color_dict: dict,
+                          style: str = 'rectangle'
+                          ) -> None:
+    """
+    Given an open image and current image data frame,
+    returns image with detection/annotation overlays.
+    :param open_img: ndarray. Represents an open image.
+    :param current_image_df: DataFrame. Represents current image detection/annotation data.
+    :param color_dict: Dictionary. Represents colors to be used in overlays.
+    :param style: String. Represents overlays style (rectangle/circle/ellipse).
+    :return: None.
+    """
+    # getting df rows
+    df_rows = current_image_df.iterrows()
+
+    # iterating over df_rows
+    for row_index, row_data in df_rows:
+
+        # adding current row overlay
+        add_single_overlay(open_img=open_img,
+                           obbs_df_row=row_data,
+                           color_dict=color_dict,
+                           style=style)
+
+
+def add_overlays_to_single_image(image_name: str,
+                                 image_path: str,
+                                 merged_df: DataFrame,
+                                 detection_threshold: float,
+                                 output_path: str,
+                                 color_dict: dict,
+                                 style: str = 'rectangle'
+                                 ) -> None:
+    """
+    Given an image name and path, and a merged
+    detections/annotations data frame, save image
+    with overlays in given output path.
+    :param image_name: String. Represents an image name.
+    :param image_path: String. Represents a file path.
+    :param merged_df: DataFrame. Represents detections/annotations data.
+    :param detection_threshold: Float. Represents detection threshold to be applied as filter.
+    :param output_path: String. Represents a file path.
+    :param color_dict: Dictionary. Represents colors to be used in overlays.
+    :param style: String. Represents overlays style (rectangle/circle/ellipse).
+    :return: None.
+    """
+    # opening image
+    open_img = imread(image_path)
+    open_img = cvtColor(open_img, COLOR_BGR2RGB)
+
+    # getting image data from df
+    current_image_df = merged_df[merged_df['img_file_name'] == image_name]
+
+    # filtering current image data based on detection threshold
+    current_image_df = current_image_df[current_image_df['detection_threshold'] >= detection_threshold]
+
+    # getting current image detection/annotation counts
+    detections = current_image_df[current_image_df['evaluator'] == 'model']
+    annotations = current_image_df[current_image_df['evaluator'] == 'fornma']
+    detection_count = len(detections)
+    annotation_count = len(annotations)
+
+    # defining base texts
+    model_text = f'model: {detection_count}'
+    fornma_text = f'fornma: {annotation_count}'
+    threshold_text = f'DT: {detection_threshold}'
+
+    # adding base texts to image corner
+    putText(open_img,
+            model_text,
+            (10, 30),
+            FONT_HERSHEY_SIMPLEX,
+            0.9,
+            color_dict['model'],
+            2)
+    putText(open_img,
+            fornma_text,
+            (10, 60),
+            FONT_HERSHEY_SIMPLEX,
+            0.9,
+            color_dict['fornma'],
+            2)
+    putText(open_img,
+            threshold_text,
+            (10, 90),
+            FONT_HERSHEY_SIMPLEX,
+            0.9,
+            color_dict['DT'],
+            2)
+
+    # adding overlays to image
+    add_multiple_overlays(open_img=open_img,
+                          current_image_df=current_image_df,
+                          color_dict=color_dict,
+                          style=style)
+
+    # saving image in output path
+    open_img = cvtColor(open_img, COLOR_RGB2BGR)
+    imwrite(output_path, open_img)
+
+
+def add_overlays_to_multiple_images(input_folder: str,
+                                    images_extension: str,
+                                    detection_file_path: str or None,
+                                    ground_truth_file_path: str or None,
+                                    output_folder: str,
+                                    detection_threshold: float,
+                                    color_dict: dict,
+                                    style: str
+                                    ) -> None:
+    """
+    Given a path to a folder containing images,
+    a path to a file containing detection info from
+    mentioned images, and a path to an output folder,
+    generates images with detection outlines (if centroid
+    flag is False, or centroids, if flag is True), saving
+    new outlined images into output folder.
+    :param input_folder: String. Represents a folder path.
+    :param images_extension: String. Represents image extension.
+    :param detection_file_path: String. Represents a file path.
+    :param ground_truth_file_path: String. Represents a file path.
+    :param output_folder: String. Represents a folder path.
+    :param detection_threshold: Float. Represents detection threshold to be applied as filter.
+    :param color_dict: Dictionary. Represents colors to be used in overlays.
+    :param style: String. Represents overlays style (rectangle/circle/ellipse).
+    :return: None.
+    """
+    # getting merged detections df
+    merged_df = get_merged_detection_annotation_df(detections_df_path=detection_file_path,
+                                                   annotations_df_path=ground_truth_file_path)
+
+    # getting images in input folder
+    images = get_specific_files_in_folder(path_to_folder=input_folder,
+                                          extension=images_extension)
+    images_num = len(images)
+    images_names = [image_name.replace(images_extension, '')
+                    for image_name
+                    in images]
+
+    # iterating over images_names
+    for image_index, image_name in enumerate(images_names, 1):
+
+        # TODO: check image correspondence between different dfs!
+
+        # printing execution message
+        progress_base_string = f'adding overlays to image #INDEX# of #TOTAL#'
+        print_progress_message(base_string=progress_base_string,
+                               index=image_index,
+                               total=images_num)
+
+        # getting image path
+        image_name_w_extension = f'{image_name}{images_extension}'
+        image_path = join(input_folder, image_name_w_extension)
+
+        # getting output path
+        try:
+            detection_origin = detection_file_path.split('/')[-1].replace('.csv', '')
+        except AttributeError:
+            detection_origin = ground_truth_file_path.split('/')[-1].replace('.csv', '')
+        output_name = f'{image_name}_overlays_{detection_origin}.png'
+        output_path = join(output_folder, output_name)
+
+        # adding overlays to current image
+        add_overlays_to_single_image(image_name=image_name,
+                                     image_path=image_path,
+                                     merged_df=merged_df,
+                                     detection_threshold=detection_threshold,
+                                     output_path=output_path,
+                                     color_dict=color_dict,
+                                     style=style)
+
+    # printing execution message
+    f_string = f'overlays added to all {images_num} images!'
+    print(f_string)
+
+######################################################################
+# defining main function
+
+
+def main():
+    """Runs main code."""
+    # getting args dict
+    args_dict = get_args_dict()
+
+    # getting input folder
+    input_folder = args_dict['input_folder']
+
+    # getting image extension
+    images_extension = args_dict['images_extension']
+
+    # getting detection file path
+    detection_file = args_dict['detection_file']
+
+    # getting ground-truth file path
+    ground_truth_file = args_dict['ground_truth_file']
+
+    # getting output folder
+    output_folder = args_dict['output_folder']
+
+    # getting overlays style
+    overlays_style = args_dict['overlays_style']
+
+    # getting detection threshold
+    detection_threshold = args_dict['detection_threshold']
+    detection_threshold = float(detection_threshold)
+
+    # printing execution parameters
+    print_execution_parameters(params_dict=args_dict)
+
+    # waiting for user input
+    enter_to_continue()
+
+    # running add_overlays_to_multiple_images function
+    add_overlays_to_multiple_images(input_folder=input_folder,
+                                    images_extension=images_extension,
+                                    detection_file_path=detection_file,
+                                    ground_truth_file_path=ground_truth_file,
+                                    output_folder=output_folder,
+                                    detection_threshold=detection_threshold,
+                                    color_dict=COLOR_DICT,
+                                    style=overlays_style)
+
+######################################################################
+# running main function
+
+
+if __name__ == '__main__':
+    main()
+
+
+######################################################################
+# end of current module
---
+++ src/utils/get_image_clusters_test.py
@@ -0,0 +1,170 @@
+# for loading/processing the images
+from keras.utils import load_img
+from keras.utils import img_to_array
+from keras.applications.vgg16 import preprocess_input
+
+from src.utils.aux_funcs import is_using_gpu
+
+# models
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
+
+# clustering and dimension reduction
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+# for everything else
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from random import randint
+import pandas as pd
+import pickle
+
+path = '/mnt/sdb/angelo-dados/pycharm_projects/single_cell_analyser/data/clustering_tests/flowers_dataset/flower_images/flower_images'
+# change the working directory to the path where the images are located
+os.chdir(path)
+
+print('Using GPU:', is_using_gpu())
+
+# this list holds all the image filename
+flowers = []
+
+# creates a ScandirIterator aliased as files
+with os.scandir(path) as files:
+    # loops through each file in the directory
+    for file in files:
+        if file.name.endswith('.png'):
+            # adds only the image files to the flowers list
+            flowers.append(file.name)
+
+f_string = f'{len(flowers)} images found'
+print(f_string)
+
+model = VGG16()
+model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+
+
+def extract_features(file, model):
+    # load the image as a 224x224 array
+    img = load_img(file, target_size=(224, 224))
+    # convert from 'PIL.Image.Image' to numpy array
+    img = np.array(img)
+    # reshape the data for the model reshape(num_of_samples, dim 1, dim 2, channels)
+    reshaped_img = img.reshape(1, 224, 224, 3)
+    # prepare image for model
+    imgx = preprocess_input(reshaped_img)
+    # get the feature vector
+    features = model.predict(imgx,
+                             use_multiprocessing=True)
+    return features
+
+
+data = {}
+p = '/mnt/sdb/angelo-dados/pycharm_projects/single_cell_analyser/data/clustering_tests/flowers_dataset/flower_images/flower_images/features.pickle'
+
+# loop through each image in the dataset
+for flower in flowers:
+    # try to extract the features and update the dictionary
+    try:
+        feat = extract_features(flower, model)
+        data[flower] = feat
+    # if something fails, save the extracted features as a pickle file (optional)
+    except:
+        with open(p, 'wb') as file:
+            pickle.dump(data, file)
+
+# get a list of the filenames
+filenames = np.array(list(data.keys()))
+
+# get a list of just the features
+feat = np.array(list(data.values()))
+
+# reshape so that there are 210 samples of 4096 vectors
+feat = feat.reshape(-1, 4096)
+
+# reduce the amount of dimensions in the feature vector
+pca = PCA(n_components=2,
+          random_state=53)  # seed
+pca.fit(feat)
+x = pca.transform(feat)
+
+# cluster feature vectors
+kmeans = KMeans(n_clusters=15,  # if we know how many groups there are
+                random_state=53,  # seed
+                n_init=10)  # kind of a bootstrap
+kmeans.fit(x)
+
+# holds the cluster id and the images { id: [images] }
+groups = {}
+for file, cluster in zip(filenames, kmeans.labels_):
+    if cluster not in groups.keys():
+        groups[cluster] = []
+        groups[cluster].append(file)
+    else:
+        groups[cluster].append(file)
+
+labels = kmeans.labels_
+
+label0 = x[labels == 0]
+label0x = label0[:, 0]
+label0y = label0[:, 1]
+print(label0)
+print(label0x)
+print(label0y)
+print(x.shape)
+
+# Getting unique labels
+
+u_labels = np.unique(labels)
+
+# plotting the results:
+
+for i in u_labels:
+    plt.scatter(x[labels == i, 0], x[labels == i, 1], label=i)
+plt.legend()
+plt.show()
+plt.close()
+
+
+# function that lets you view a cluster (based on identifier)
+def view_cluster(cluster):
+    plt.figure(figsize=(25, 25))
+    # gets the list of filenames for a cluster
+    files = groups[cluster]
+    # only allow up to 30 images to be shown at a time
+    if len(files) > 30:
+        print(f"Clipping cluster size from {len(files)} to 30")
+        files = files[:29]
+    # plot each image in the cluster
+    for index, file in enumerate(files):
+        plt.subplot(10, 10, index + 1)
+        img = load_img(file)
+        img = np.array(img)
+        plt.imshow(img)
+        plt.axis('off')
+    plt.show()
+
+
+view_cluster(8)
+
+# this is just incase you want to see which value for k might be the best
+sse = []
+list_k = list(range(3, 51))
+
+for k in list_k:
+    km = KMeans(n_clusters=k,
+                n_init=10,
+                random_state=53)
+    km.fit(x)
+
+    sse.append(km.inertia_)
+
+# Plot sse against k
+plt.figure(figsize=(6, 6))
+plt.plot(list_k, sse)
+plt.xlabel(r'Number of clusters *k*')
+plt.ylabel('Sum of squared distance')
+plt.show()
+
+# end of current module
