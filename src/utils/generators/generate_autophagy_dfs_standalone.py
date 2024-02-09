0ed149a62ed38cd483631f49c1993464dd7ad132
angelo-angonezi-win <angeloangonezi2@gmail.com>
2024-02-09 11:29:06 -0300

    created standalone version for generate_autophagy_dfs.py

--- src/utils/generators/generate_autophagy_dfs_standalone.py
+++ src/utils/generators/generate_autophagy_dfs_standalone.py
@@ -1,4 +1,4 @@
-# generate autophagy dfs module
+# generate autophagy dfs standalone module
 
 print('initializing...')  # noqa
 
@@ -11,6 +11,8 @@ print('initializing...')  # noqa
 # importing required libraries
 print('importing required libraries...')  # noqa
 from cv2 import imread
+from os import listdir
+from sys import stdout
 from cv2 import imwrite
 from cv2 import putText
 from cv2 import cvtColor
@@ -29,10 +31,6 @@ from cv2 import pointPolygonTest
 from cv2 import CHAIN_APPROX_NONE
 from argparse import ArgumentParser
 from cv2 import FONT_HERSHEY_SIMPLEX
-from src.utils.aux_funcs import enter_to_continue
-from src.utils.aux_funcs import print_progress_message
-from src.utils.aux_funcs import print_execution_parameters
-from src.utils.aux_funcs import get_specific_files_in_folder
 print('all required libraries successfully imported.')  # noqa
 
 ######################################################################
@@ -101,6 +99,191 @@ def get_args_dict() -> dict:
 
     # returning the arguments dictionary
     return args_dict
+
+######################################################################
+# auxiliary functions from aux_funcs module
+
+
+def spacer(char: str = '_',
+           reps: int = 50
+           ) -> None:
+    """
+    Given a char and a number of reps,
+    prints a "spacer" string assembled
+    by multiplying char by reps.
+    :param char: String. Represents a character to be used
+    as basis for spacer.
+    :param reps: Integer. Represents number of character's
+    repetitions used in spacer.
+    :return: None.
+    """
+    # defining spacer string
+    spacer_str = char * reps
+
+    # printing spacer string
+    print(spacer_str)
+
+
+def flush_string(string: str) -> None:
+    """
+    Given a string, writes and flushes it in the console using
+    sys library, and resets cursor to the start of the line.
+    (writes N backspaces at the end of line, where N = len(string)).
+    :param string: String. Represents a message to be written in the console.
+    :return: None.
+    """
+    # getting string length
+    string_len = len(string)
+
+    # creating backspace line
+    backspace_line = '\b' * string_len
+
+    # writing string
+    stdout.write(string)
+
+    # flushing console
+    stdout.flush()
+
+    # resetting cursor to start of the line
+    stdout.write(backspace_line)
+
+
+def flush_or_print(string: str,
+                   index: int,
+                   total: int
+                   ) -> None:
+    """
+    Given a string, prints string if index
+    is equal to total, and flushes it on console
+    otherwise.
+    !(useful for progress tracking/progress bars)!
+    :param string: String. Represents a string to be printed on console.
+    :param index: Integer. Represents an iterable's index.
+    :param total: Integer. Represents an iterable's total.
+    :return: None.
+    """
+    # checking whether index is last
+
+    # if current element is last
+    if index == total:
+
+        # printing string
+        print(string)
+
+    # if current element is not last
+    else:
+
+        # flushing string
+        flush_string(string)
+
+
+def print_progress_message(base_string: str,
+                           index: int,
+                           total: int
+                           ) -> None:
+    """
+    Given a base string (containing keywords #INDEX#
+    and #TOTAL#) an index and a total (integers),
+    prints execution message, substituting #INDEX#
+    and #TOTAL# keywords by respective integers.
+    !!!Useful for FOR loops execution messages!!!
+    :param base_string: String. Represents a base string.
+    :param index: Integer. Represents an execution index.
+    :param total: Integer. Represents iteration maximum value.
+    :return: None.
+    """
+    # getting percentage progress
+    progress_ratio = index / total
+    progress_percentage = progress_ratio * 100
+    progress_percentage_round = round(progress_percentage)
+
+    # assembling progress string
+    progress_string = base_string.replace('#INDEX#', str(index))
+    progress_string = progress_string.replace('#TOTAL#', str(total))
+    progress_string += '...'
+    progress_string += f' ({progress_percentage_round}%)'
+    progress_string += '     '
+
+    # showing progress message
+    flush_or_print(string=progress_string,
+                   index=index,
+                   total=total)
+
+
+def get_specific_files_in_folder(path_to_folder: str,
+                                 extension: str
+                                 ) -> list:
+    """
+    Given a path to a folder, returns a list containing
+    all files in folder that match given extension.
+    :param path_to_folder: String. Represents a path to a folder.
+    :param extension: String. Represents a specific file extension.
+    :return: List[str]. Represents all files that match extension in given folder.
+    """
+    # getting all files in folder
+    all_files_in_folder = listdir(path_to_folder)
+
+    # getting specific files
+    files_in_dir = [file                          # getting file
+                    for file                      # iterating over files
+                    in all_files_in_folder        # in input folder
+                    if file.endswith(extension)]  # only if file matches given extension
+
+    # sorting list
+    files_in_dir = sorted(files_in_dir)
+
+    # returning list
+    return files_in_dir
+
+
+def print_execution_parameters(params_dict: dict) -> None:
+    """
+    Given a list of execution parameters,
+    prints given parameters on console,
+    such as:
+    '''
+    --Execution parameters--
+    input_folder: /home/angelo/Desktop/ml_temp/imgs/
+    output_folder: /home/angelo/Desktop/ml_temp/overlays/
+    '''
+    :param params_dict: Dictionary. Represents execution parameters names and values.
+    :return: None.
+    """
+    # defining base params_string
+    params_string = f'--Execution parameters--'
+
+    # iterating over parameters in params_list
+    for dict_element in params_dict.items():
+
+        # getting params key/value
+        param_key, param_value = dict_element
+
+        # adding 'Enter' to params_string
+        params_string += '\n'
+
+        # getting current param string
+        current_param_string = f'{param_key}: {param_value}'
+
+        # appending current param string to params_string
+        params_string += current_param_string
+
+    # printing final params_string
+    spacer()
+    print(params_string)
+    spacer()
+
+
+def enter_to_continue():
+    """
+    Waits for user input ("Enter")
+    and once press, continues to run code.
+    """
+    # defining enter_string
+    enter_string = f'press "Enter" to continue'
+
+    # waiting for user input
+    input(enter_string)
+
 
 ######################################################################
 # defining auxiliary functions
