d92cafe6ce4d24c2d2ce34e9f2edb96e8dde7c97
angelo-angonezi-win <angeloangonezi2@gmail.com>
2024-03-29 21:33:43 -0300

    updated vgg based architecture

--- src/ml/classification/binary/train.py
+++ src/ml/classification/binary/train.py
@@ -219,7 +219,7 @@ def get_vgg16_model(input_shape: tuple) -> Sequential:
     for layer_index, layer in enumerate(base_layers):
 
         # checking layer index
-        if layer_index < 15:
+        if layer_index < 20:
 
             # freezing layer
             layer.trainable = False
