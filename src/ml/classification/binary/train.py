e473e420e720f4852d244466e46e73200815fc83
angelo-angonezi-win <angeloangonezi2@gmail.com>
2024-04-09 22:57:36 -0300

    updated new architecture

--- src/ml/classification/binary/train.py
+++ src/ml/classification/binary/train.py
@@ -379,7 +379,7 @@ def get_new_model(input_shape: tuple) -> Sequential:
     model.add(Conv2D(32, (3, 3), input_shape=input_shape, activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(32, (3, 3), 'relu'))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Conv2D(64, (3, 3), activation='relu'))
