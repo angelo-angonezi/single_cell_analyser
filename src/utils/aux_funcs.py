9ed4cc93d58fc098188d1c1cb0e9eb29885ca70d
angelo-angonezi-win <angeloangonezi2@gmail.com>
2024-05-03 11:08:00 -0300

    added alternative vgg based architecture

--- src/utils/aux_funcs.py
+++ src/utils/aux_funcs.py
@@ -1498,10 +1498,6 @@ def get_data_split_from_df(splits_folder: str,
                                                      class_mode=class_mode,  # 'raw' for regression, 'binary' for binary classification and 'categorical' for multi-label classification  # noqa
                                                      batch_size=batch_size,
                                                      shuffle=True)
-    print(split_data)
-    print(split_data[0])
-    print(split_data[0][0])
-    exit()
 
     # returning data
     return split_data
